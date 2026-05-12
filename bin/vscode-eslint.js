4a5888ec27f9283599911a0e9a777882d5f23485
Shubham Mishra <31804728+shubham151@users.noreply.github.com>
2026-05-12 16:16:55 -0700

    Add vscode-eslint CLI bin entry point

---
+++ bin/vscode-eslint.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+console.log('vscode-eslint: this repository publishes a VS Code extension, not a standalone CLI.');
+console.log('Install the extension in VS Code instead.');
+
+process.exitCode = 0;
